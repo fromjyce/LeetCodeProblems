a0bc2f01891706df34aae8990f834491904554e3
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-10-02 21:36:46 +0530

    Delete BasicPrograms/Palindrome.py

--- BasicPrograms/Palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        n = str(x)
-        rev = n[::-1]
-        if rev==n:
-            return true
-        else:
-            return false
